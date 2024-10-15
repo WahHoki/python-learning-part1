fd29e146422d893e3b544c7b4dad4b1a9c40ec29
WahHoki <wahhoki07@gmail.com>
2024-10-15 16:06:47 +0700

    Add files via upload

---
+++ inputuser.py
@@ -0,0 +1,14 @@
+# mengambil input data dari user
+# input user
+
+# data yang dimasukan pasti string
+data = input("Masukan data = ")
+print("data = ", data, ",type = ", type(data))
+
+# jika kita ingin mengambil int, maka ;
+angka = int(input("masukan angka = "))
+print("angka = ", angka, ",type = ", type(angka))
+
+# jika kita ingin mengambil boolean, maka ;
+bilangan_biner = bool(input("masukan bilangan biner = "))
+print("bilangan biner = ", bilangan_biner,",type = ", type(bilangan_biner))
