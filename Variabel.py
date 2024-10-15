564044dabc06f4fe85f84dcb1142eb07aa6974dc
WahHoki <wahhoki07@gmail.com>
2024-10-15 14:35:05 +0700

    Add files via upload

---
+++ Variabel.py
@@ -0,0 +1,28 @@
+#  (x) = Variabel, (=) = assignment, (10) = value
+a = 10
+
+# Variabel adalah tempat menyimpan data
+
+# Menaruh / assignment nilai
+a = 9
+print("Nilai a = ", a)
+
+x = 3
+print("Nilai x = ", x)
+
+length = 600
+print("Nilai length = ", length)
+
+# penamaan
+nilai_x = 15 # Penggunaan underscore
+nilaiX = 8 # Diperbolehkan
+Ribu10 = 10000 # Penggunaan angka tidak boleh didepan meskipun menggunakan underscore
+
+# Pemanggilan kedua
+print("Nilai a =", a)
+a = 7
+print("Nilai a =", a)
+
+# assignment indirect
+b = a 
+print("Nilai b =", b)
