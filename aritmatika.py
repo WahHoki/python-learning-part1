ae54421ba917f9d09259d5e88a34447841938683
WahHoki <wahhoki07@gmail.com>
2024-10-17 14:45:10 +0700

    Add files via upload

---
+++ aritmatika.py
@@ -0,0 +1,42 @@
+# operasi aritmatika
+
+a = 10
+b = 15
+
+# operasi tambah +
+hasil = a + b 
+print (a,'+',b,'=',hasil)
+
+# operasi pengurangan -
+hasil = a - b
+print (a,'-',b,'=',hasil)
+
+# operasi kali *
+hasil = a * b
+print (a,'x',b,'=',hasil)
+
+# operasi bagi /
+hasil = a / b
+print (a,'/',b,'=',hasil)
+
+# operasi eksponen (pangkat) **
+hasil = a ** b
+print( a, '**', b, '=', hasil)
+
+# operasi modulus %
+hasil = a % b
+print( a, '%', b, '=', hasil)
+
+# operasi //
+hasil = a // b
+print (a,'//',b,'=',hasil)
+
+
+
+## merubah semua menjadi upper case
+bre = "bALer bRo"
+print("hasil = " + bre)
+bre = bre.upper()
+print("b = " + bre)
+bre = bre.lower()
+print("a = " + bre)
